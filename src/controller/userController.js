536424a5e744460f43b6d66071508515f00bfec6
Matari73 <marianadeoliveira73@hotmail.com>
2024-08-19 21:59:44 -0300

    Adicionando rota de login

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -20,6 +20,30 @@ class UserController {
         }
     }
 
+    static async login(req, res) {
+        const { email, senha } = req.body;
+    
+        if (!email || !senha) {
+            return res.status(400).json({ message: "Email e senha são obrigatórios." });
+        }
+    
+        try {
+            const userEncontrado = await user.findOne({ email });
+            if (!userEncontrado) {
+                return res.status(404).json({ message: "Usuário não encontrado." });
+            }
+    
+            const senhaIguais = await userEncontrado.comparePassword(senha);
+            if (!senhaIguais) {
+                return res.status(401).json({ message: "Senha incorreta." });
+            }
+
+            res.status(200).json({ message: "Login bem-sucedido!", user: { email: userEncontrado.email, nome: userEncontrado.nome } });
+        } catch (error) {
+            console.error("Erro durante o login:", error.message);
+            res.status(500).json({ message: `${error.message} - falha ao fazer login.` });
+        }
+    }
 
 }
 
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,5 +4,6 @@ import UserController from "../controller/userController.js";
 const routes = express.Router()
 
 routes.post("/user", UserController.criarUser)
+routes.post("/login", UserController.login)
 
 export default routes;

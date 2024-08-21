a47f62ccd6aefcf1be747b7e682c2935f91d882b
Matari73 <marianadeoliveira73@hotmail.com>
2024-08-21 20:12:24 -0300

    Adicionando logs no userController

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,5 +1,6 @@
 import user from "../models/User.js";
 import jwt from "jsonwebtoken";
+import logger from "../services/logger.js";
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
@@ -8,16 +9,18 @@ class UserController {
     static async criarUser(req, res) {
         try {
             const novoUser = await user.create(req.body);
+            logger.info("User criado com sucesso", { user: novoUser });
             res.status(201).json({ message: "Criado com sucesso.", user: novoUser });
         } catch (erro) {
-            if (erro.name === 'ValidationError') {
+            if (erro.name === "ValidationError") {
                 const mensagensErro = Object.values(erro.errors).map(err => err.message);
-                res.status(400).json({ message: mensagensErro.join(', ') });
-            }
-            else if (erro.code === 11000) {
+                logger.error("Erro de validação:", { errors: mensagensErro, originalError: erro.message });
+                res.status(400).json({ message: mensagensErro.join(", ") });
+            } else if (erro.code === 11000) {
+                logger.error("Email já cadastrado.", { error: erro.message });
                 res.status(409).json({ message: "Email já cadastrado." });
-            }
-            else {
+            } else {
+                logger.error("Falha ao criar usuário.", { error: erro.message, stack: erro.stack });
                 res.status(500).json({ message: `${erro.message} - falha ao criar usuário.` });
             }
         }
@@ -27,30 +30,32 @@ class UserController {
         const { email, senha } = req.body;
 
         if (!email || !senha) {
+            logger.error("Email ou senha não inseridos");
             return res.status(400).json({ message: "Email e senha são obrigatórios." });
         }
 
         try {
             const userEncontrado = await user.findOne({ email });
             if (!userEncontrado) {
+                logger.error("Usuário não encontrado com o email:", { email });
                 return res.status(404).json({ message: "Usuário não encontrado." });
             }
 
             const senhaIguais = await userEncontrado.comparePassword(senha);
             if (!senhaIguais) {
+                logger.error("Senha incorreta para o email:", { email });
                 return res.status(401).json({ message: "Senha incorreta." });
             }
 
             const token = jwt.sign({ id: userEncontrado._id, email: userEncontrado.email }, SECRET_KEY, { expiresIn: '15m' });
             res.cookie('auth-token', token, { httpOnly: true, secure: false });
-
+            logger.info("Login bem sucedido", { user: { email: userEncontrado.email, nome: userEncontrado.nome } });
             res.status(200).json({
-                message: "Login bem-sucedido!",
+                message: "Login bem sucedido!",
                 user: { email: userEncontrado.email, nome: userEncontrado.nome },
-                token
             });
         } catch (error) {
-            console.error("Erro durante o login:", error.message);
+            logger.error("Erro durante o login:", { error: error.message, stack: error.stack });
             res.status(500).json({ message: `${error.message} - falha ao fazer login.` });
         }
     }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,16 +3,32 @@ import cookieParser from "cookie-parser";
 import personagens from "./personagensRoutes.js";
 import users from "./usersRoutes.js";
 import limiter from "../middleware/limiter.js";
+import logger from "../services/logger.js";
+import expressWinston from "express-winston";
 
 const routes = (app) => {
     app.use(express.json());
     app.use(limiter);
     app.use(cookieParser());
-
-    app.route("/").get((req, res) => res.status(200).send("Projeto 3 - Back-end"));
-
     app.use(personagens);
     app.use(users);
+    app.use(expressWinston.logger({
+        winstonInstance: logger,
+        meta: true,
+        msg: "HTTP {{req.method}} {{req.url}}",
+        expressFormat: true,
+        colorize: false,
+    }));
+
+    app.use(expressWinston.errorLogger({
+        winstonInstance: logger,
+        meta: true,
+        msg: "Erro {{err.message}}",
+        expressFormat: true,
+        colorize: false,
+    }));
+
+    app.route("/").get((req, res) => res.status(200).send("Projeto 3 - Back-end"));
 };
 
 export default routes;

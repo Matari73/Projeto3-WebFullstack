9509bdddafcd1ef574cd1bc7523c5c0c5daa0674
Matari73 <marianadeoliveira73@hotmail.com>
2024-08-21 19:08:17 -0300

    Adicionando quantidade limite de requisições

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import personagens from "./personagensRoutes.js";
 import users from "./usersRoutes.js";
-import cookieParser from "cookie-parser";
+import limiter from "../middleware/limiter.js";
 
 const routes = (app) => {
     app.use(express.json());
+    app.use(limiter);
     app.use(cookieParser());
 
     app.route("/").get((req, res) => res.status(200).send("Projeto 3 - Back-end"));

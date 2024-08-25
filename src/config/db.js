897101e63f3e71e1bdb58c9fab080e3230f2d419
Matari73 <marianadeoliveira73@hotmail.com>
2024-08-25 10:29:58 -0300

    Adicionando configs de pool de conexões

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const options = {
+    MaxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+};
+
 async function conectaNaDatabase() {
-    mongoose.connect(process.env.DB_CONNECTION_STR);
-    return mongoose.connection;
+    try {
+        await mongoose.connect(process.env.DB_CONNECTION_STR, options);
+        console.log("Conexão com o MongoDB estabelecida com sucesso.");
+        return mongoose.connection;
+    } catch (err) {
+        console.error("Erro ao conectar ao MongoDB:", err);
+        process.exit(1);
+    }
 }
-  
+
 export default conectaNaDatabase;
